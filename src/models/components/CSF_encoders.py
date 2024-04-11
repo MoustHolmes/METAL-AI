29369042aa43a150357196c2d75a0797c0aa1c5f
mouhol mouhol <mouhol@g-15-g0043.cm.cluster>
2024-04-11 22:20:04 +0200

    first running version

---
+++ src/models/components/CSF_encoders.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+
+class simple_CSF_encoder(nn.Module):
+    def __init__(self, output_size=32):
+        super(simple_CSF_encoder, self).__init__()
+        # Assuming the input size is 6 because we append n_electrons (1) and n_protons (1) to each 4-dimensional CSF.
+        self.network = nn.Sequential(
+        nn.Linear(6, 64),
+        nn.ReLU(),
+        nn.Linear(64, output_size)
+        )
+
+    def forward(self, csfs, n_electrons, n_protons):
+        # Append n_electrons and n_protons to each CSF
+        n_electrons = n_electrons.unsqueeze(-1).unsqueeze(-1).expand(-1, csfs.size(1), 1)
+        n_protons = n_protons.unsqueeze(-1).unsqueeze(-1).expand(-1, csfs.size(1), 1)
+        extended_csfs = torch.cat([csfs, n_electrons, n_protons], dim=-1)
+        return self.network(extended_csfs)
