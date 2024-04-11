2c74a56706c43aac9198fc4856bfeb4c01e759a1
mouhol mouhol <mouhol@g-15-g0043.cm.cluster>
2024-04-11 22:20:04 +0200

    first running version

---
+++ src/loss_functions/loss_function_wrappers.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class EffectSumLosswrapper(nn.Module):
+    def __init__(self, loss_function=F.mse_loss):
+        super(EffectSumLosswrapper, self).__init__()
+        self.loss_function = loss_function
+        
+    def forward(self, pred, target, mask):
+        mask = mask.bool()
+        valid_pred = ~torch.isnan(pred)
+        valid_target = ~torch.isnan(target)
+        valid_elements = valid_pred & valid_target & ~mask
+        filtered_pred = pred[valid_elements]
+        filtered_target = target[valid_elements]
+        loss = self.loss_function(filtered_pred, filtered_target)
+        if loss.numel() > 1:
+            loss = loss.sum()
+        return loss
+    
+class EffectMeanLossWrapper(nn.Module):
+    def __init__(self, loss_function=F.mse_loss):
+        super(EffectMeanLossWrapper, self).__init__()
+        self.loss_function = loss_function
+        
+    def forward(self, pred, target, mask):
+        mask = mask.bool()
+        valid_pred = ~torch.isnan(pred)
+        valid_target = ~torch.isnan(target)
+        valid_elements = valid_pred & valid_target & ~mask
+        filtered_pred = pred[valid_elements]
+        filtered_target = target[valid_elements]
+        loss = self.loss_function(filtered_pred, filtered_target)
+        if loss.numel() > 1:
+            loss = loss.mean()
+        return loss
+    
+class ConvergedLossWrapper(nn.Module):
+    def __init__(self, loss_function=F.cross_entropy):
+        super(ConvergedLossWrapper, self).__init__()
+        self.loss_function = loss_function
+        
+    def forward(self, pred, target, converge_index):
+        loss = self.loss_function(pred[converge_index], target[converge_index])
+        return loss
---
+++ src/models/components/Transformer_encoder_model.py
@@ -0,0 +1,30 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class simple_transformer_encoder_model(nn.Module):
+    def __init__(
+            self, 
+            csf_encoder,         
+            input_size: int = 6,
+            d_model: int = 64, 
+            nhead: int = 8,
+            dim_forward: int = 64,
+            num_layers: int = 6,
+            output_size = 1, 
+            dropout=0.5
+        ):
+            super(simple_transformer_encoder_model, self).__init__()
+            self.csf_encoder = csf_encoder
+            encoder_layers = nn.TransformerEncoderLayer(d_model, nhead, dim_forward, dropout, batch_first=True)
+            self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers)
+            self.decoder = nn.Linear(d_model, output_size)
+
+
+    def forward(self, input_dict):
+        csfs = input_dict['excitations']
+        encoded_csfs = self.csf_encoder(csfs, input_dict['n_electrons'], input_dict['n_protons'])
+        output = self.transformer_encoder(encoded_csfs,src_key_padding_mask = input_dict['mask'])#src_key_padding_mask = ~input_dict['mask']
+        # output = output[:, 0, :]
+        output = self.decoder(output)
+        return F.softplus(output).squeeze(-1)
